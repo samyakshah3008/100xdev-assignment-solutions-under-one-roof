adfbb67261785d90d474b85a0f5b44e8b72cc3a2
Samyak Shah <samyakshah3008@gmail.com>
2024-06-15 18:42:06 +0530

    Assignment 4 sol

---
+++ week-6/src/Assignment4.jsx
@@ -0,0 +1,40 @@
+import { useCallback, useState } from "react";
+
+// Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
+
+export default function Assignment4() {
+  const [count, setCount] = useState(0);
+
+  // Your code starts here
+
+  const cachedHandleIncrement = useCallback(
+    function handleIncrement() {
+      setCount((count) => count + 1);
+    },
+    [count]
+  );
+  const cachedHandleDecrement = useCallback(
+    function handleDecrement() {
+      setCount((count) => count - 1);
+    },
+    [count]
+  );
+  // Your code ends here
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <CounterButtons
+        onIncrement={cachedHandleIncrement}
+        onDecrement={cachedHandleDecrement}
+      />
+    </div>
+  );
+}
+
+const CounterButtons = ({ onIncrement, onDecrement }) => (
+  <div>
+    <button onClick={onIncrement}>Increment</button>
+    <button onClick={onDecrement}>Decrement</button>
+  </div>
+);
