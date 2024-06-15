adacfd8a318d3cd44a6d6146baa3e5f0210ccce9
Samyak Shah <samyakshah3008@gmail.com>
2024-06-16 01:33:33 +0530

    initial setup week-7

--- week-6/src/App.js
+++ week-6/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
-import Assignment1 from "./Assignment1";
+import Assignment4 from "./Assignment4";
 
 function App() {
   return (
     <div className="App">
       <h1>Samyak</h1>
-      <Assignment1 />
+      {/* <Assignment1 /> */}
+      {/* <Assignment4 /> */}
+      {/* <Assignment3 /> */}
+      <Assignment4 />
     </div>
   );
 }
---
+++ week-7/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Samyak Shah</h1>
+    </div>
+  );
+}
+
+export default App;
